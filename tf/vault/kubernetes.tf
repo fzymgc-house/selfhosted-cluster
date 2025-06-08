6c9c74f876700b37049fba5f49dc86f803329dad
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-08 17:02:11 -0400

    config k8s auth

--- tf/vault/kubernetes.tf
+++ tf/vault/kubernetes.tf
@@ -6,3 +6,13 @@ import {
 resource "vault_auth_backend" "kubernetes" {
   type = "kubernetes"
 }
+
+import {
+  id = "auth/kubernetes"
+  to = vault_kubernetes_auth_backend_config.kubernetes
+}
+
+resource "vault_kubernetes_auth_backend_config" "kubernetes" {
+  backend = vault_auth_backend.kubernetes.path
+  kubernetes_host = "https://kubernetes.default.svc"
+}

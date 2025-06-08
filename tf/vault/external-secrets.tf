af3f1db3a3a4ea46e55acf4f594e26cc2b6b45b8
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-08 09:48:54 -0400

    fix eso ref

---
+++ tf/vault/external-secrets.tf
@@ -0,0 +1,26 @@
+resource "vault_kubernetes_auth_backend_role" "external-secrets" {
+  backend = data.vault_kubernetes_auth_backend_config.kubernetes.path
+  role_name = "external-secrets"
+  bound_service_account_namespaces = ["external-secrets"]
+  bound_service_account_names = ["external-secrets"]
+  audience = "vault"
+  token_policies = ["default", "external-secrets-operator"]
+}
+
+import {
+  to = vault_policy.external-secrets-operator
+  id = "external-secrets-operator"
+}
+
+resource "vault_policy" "external-secrets-operator" {
+  name   = "external-secrets-operator"
+  policy = <<EOT
+path "secret/data" {
+  capabilities = ["read","list"]
+}
+
+path "secret/data/*" {
+  capabilities = ["read","list"]
+}
+EOT
+}

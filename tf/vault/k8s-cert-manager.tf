f35d573454a2b093a98cbcbea8848ae0680a750b
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-08 20:25:53 -0400

    vault cluster issuer

---
+++ tf/vault/k8s-cert-manager.tf
@@ -0,0 +1,13 @@
+import {
+  id = "auth/kubernetes/role/cert-manager"
+  to = vault_kubernetes_auth_backend_role.cert-manager
+}
+
+resource "vault_kubernetes_auth_backend_role" "cert-manager" {
+  backend = vault_auth_backend.kubernetes.path
+  role_name = "cert-manager"
+  bound_service_account_namespaces = ["cert-manager"]
+  bound_service_account_names = ["cert-manager"]
+  audience = "vault://vault"
+  token_policies = ["default", "cert-manager"]
+}

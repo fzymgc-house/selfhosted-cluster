025b0969372f1c88263d0a8b198c28b4c7859d6e
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-08 21:21:23 -0400

    add sa

--- tf/vault/k8s-cert-manager.tf
+++ tf/vault/k8s-cert-manager.tf
@@ -7,7 +7,7 @@ resource "vault_kubernetes_auth_backend_role" "cert-manager" {
   backend = vault_auth_backend.kubernetes.path
   role_name = "cert-manager"
   bound_service_account_namespaces = ["cert-manager"]
-  bound_service_account_names = ["cert-manager"]
+  bound_service_account_names = ["cert-manager","vault-issuer"]
   audience = "vault://vault"
   token_policies = ["default", "cert-manager"]
 }

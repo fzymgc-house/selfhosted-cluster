68979969f3e1a5abde9c50ca65ad7399aae4b088
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-11 20:08:31 -0400

    turn on vault ldap

--- tf/authelia/lldap.tf
+++ tf/authelia/lldap.tf
@@ -2,7 +2,7 @@ resource "vault_ldap_auth_backend" "lldap" {
   path = "ldap"
   url = "ldaps://lldap.lldap"
   binddn = "cn=admin,dc=fzymgc,dc=house"
-  bindpass = var.vault_ldap_admin_password
+  bindpass = var.ldap_admin_password
   userdn = "ou=users,dc=fzymgc,dc=house"
   userattr = "uid"
   groupdn = "ou=groups,dc=fzymgc,dc=house"
--- tf/authelia/variables.tf
+++ tf/authelia/variables.tf
@@ -1,6 +1,6 @@
 // variables.tf - Input variables for core-services module
 
-variable "vault_ldap_admin_password" {
+variable "ldap_admin_password" {
   type = string
   description = "The password for the LDAP admin user"
 }

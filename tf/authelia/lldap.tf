3c053d895b0773d224ee41fb49262665f52eb55c
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-11 21:50:41 -0400

    add in admin username

--- tf/authelia/lldap.tf
+++ tf/authelia/lldap.tf
@@ -10,10 +10,11 @@ import {
 resource "vault_ldap_auth_backend" "lldap" {
   path = "ldap"
   url = "ldaps://lldap.lldap"
-  binddn = "cn=admin,dc=fzymgc,dc=house"
+  binddn = "cn=${var.ldap_admin_username},dc=fzymgc,dc=house"
   bindpass = var.ldap_admin_password
   userdn = "ou=users,dc=fzymgc,dc=house"
   userattr = "uid"
+  username_as_alias = true
   groupdn = "ou=groups,dc=fzymgc,dc=house"
   groupattr = "cn"
   groupfilter = "(|(memberUid={{.UserDN}})(member={{.UserDN}}))"
--- tf/authelia/variables.tf
+++ tf/authelia/variables.tf
@@ -1,5 +1,10 @@
 // variables.tf - Input variables for core-services module
 
+variable "ldap_admin_username" {
+  type = string
+  description = "The username for the LDAP admin user"
+}
+
 variable "ldap_admin_password" {
   type = string
   description = "The password for the LDAP admin user"

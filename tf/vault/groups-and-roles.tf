bf788e5c0aeb96380a050af6f336d02a4c72695e
Sean Brandt <4678+seanb4t@users.noreply.github.com>
2025-06-23 19:54:34 -0400

    another fix

--- tf/vault/groups-and-roles.tf
+++ tf/vault/groups-and-roles.tf
@@ -3,15 +3,15 @@ import {
   id = "e1165c10-0592-4885-836b-865ac465c593"
 }
 
-import {
-  to = vault_identity_group.admin
-  id = "e276baa3-366a-1e4a-7afa-17503360994c"
-}
-
 resource "vault_identity_group" "reader" {
   name = "reader"
   type = "external"
   policies = ["reader"]
+}
+
+import {
+  to = vault_identity_group.admin
+  id = "e276baa3-366a-1e4a-7afa-17503360994c"
 }
 
 resource "vault_identity_group" "admin" {

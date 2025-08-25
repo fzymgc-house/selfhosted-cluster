787d5975c706babcb0f43c434ec70fee413aa978
seanb4t <4678+seanb4t@users.noreply.github.com>
2025-08-25 14:06:15 -0400

    update to use tf cloud

--- tf/vault/terraform.tf
+++ tf/vault/terraform.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "fzymgc-house"
+    workspaces {
+      tags = ["main-cluster", "vault"]
+    }
+  }
+}
+
 provider "onepassword" {
   account = "OGRXP4CXIVAVXIQ2QBBL7ZOHWE"
 }

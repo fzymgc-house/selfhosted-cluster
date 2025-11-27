a571d41b180043d4c6072a24654b9dc3fd7f444f
seanb4t <4678+seanb4t@users.noreply.github.com>
2025-11-26 20:08:52 -0500

    feat(vault): add Mealie policy and Kubernetes auth role

---
+++ tf/vault/k8s-mealie.tf
@@ -0,0 +1,14 @@
+import {
+  id = "auth/kubernetes/role/mealie"
+  to = vault_kubernetes_auth_backend_role.mealie
+}
+
+resource "vault_kubernetes_auth_backend_role" "mealie" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "mealie"
+  bound_service_account_names      = ["default"]
+  bound_service_account_namespaces = ["mealie"]
+  audience                         = "https://kubernetes.default.svc.cluster.local"
+  token_ttl                        = 3600
+  token_policies                   = ["default", vault_policy.mealie.name]
+}

ad5d341d5759d470f738e45cd710fa2c4eea46e1
sean <sean@fuzzymagic.com>
2025-12-10 01:37:08 +0000

    [WM] Script 'f/terraform/test_configuration' deployed

--- windmill/f/terraform/test_configuration.py
+++ windmill/f/terraform/test_configuration.py
@@ -141,7 +141,7 @@ def main(
     # Summary
     all_success = all(r["success"] for r in results.values() if r["tested"])
 
-    # Finitio
+    # Complete
     return {
         "overall_success": all_success,
         "results": results,
